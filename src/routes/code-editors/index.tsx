35ecad328f26fb6d1dd5115957da514298658db1
MGSimard <mgsimard.dev@gmail.com>
2025-05-15 05:43:21 -0400

    Gonna start thinking about fonts

--- src/routes/code-editors/index.tsx
+++ src/routes/code-editors/index.tsx
@@ -9,13 +9,19 @@ function RouteComponent() {
   return (
     <>
       <section id="visual-studio-code">
-        <h2>Visual Studio Code</h2>
+        <h2 className="text-2xl font-bold">Visual Studio Code</h2>
+        <hr />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
       </section>
       <section id="cursor">
-        <h2>Cursor</h2>
+        <h2 className="text-2xl font-bold">Cursor</h2>
+        <hr />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
       </section>
       <section id="phoenix-code">
-        <h2>Phoenix Code</h2>
+        <h2 className="text-2xl font-bold">Phoenix Code</h2>
+        <hr />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
       </section>
     </>
   );
