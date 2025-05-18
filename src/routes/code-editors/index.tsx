3c0bcebc896961003d45f5850d9a4791e3568bfd
MGSimard <mgsimard.dev@gmail.com>
2025-05-18 12:39:57 -0400

    Code editor descs

--- src/routes/code-editors/index.tsx
+++ src/routes/code-editors/index.tsx
@@ -17,9 +17,10 @@ function PageCodeEditors() {
         title="Visual Studio Code"
         links={[{ label: "Visual Studio Code", href: "https://code.visualstudio.com/" }]}
         docsLinks={[{ label: "Visual Studio Code", href: "https://code.visualstudio.com/docs" }]}
-        subTitle="Lorem ipsum dolor."
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+        subTitle="The industry standard."
+        description="Powerful and extensible, Visual Studio Code offers a vast marketplace of extensions, robust debugging tools, and an integrated terminal."
         features={[""]}></Section>
+
       <Section
         id="cursor"
         className="bg-black"
@@ -27,9 +28,10 @@ function PageCodeEditors() {
         title="Cursor"
         links={[{ label: "Cursor", href: "https://www.cursor.com/" }]}
         docsLinks={[{ label: "Cursor", href: "https://docs.cursor.com/" }]}
-        subTitle="Lorem ipsum dolor."
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+        subTitle="AI-loaded fork of Visual Studio Code."
+        description="As a fork of Visual Studio Code, Cursor integrates AI features to accelerate development workflows through intelligent code prediction autocomplete, coding agents and chat capabilities."
         features={[""]}></Section>
+
       <Section
         id="phoenix-code"
         className="bg-[linear-gradient(oklch(0.73_0.1779_55.91),oklch(0.61_0.2381_30.92))]"
@@ -37,8 +39,8 @@ function PageCodeEditors() {
         title="Phoenix Code"
         links={[{ label: "Phoenix Code", href: "https://phcode.dev/" }]}
         docsLinks={[{ label: "Phoenix Code", href: "https://docs.phcode.dev/" }]}
-        subTitle="Lorem ipsum dolor."
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+        subTitle="Lightweight code editor for beginners."
+        description="Simple and fast, Phoenix Code is perfect for beginners learning HTML, CSS & JavaScript, or for users who prefer a minimalist coding environment."
         features={[""]}></Section>
     </>
   );
