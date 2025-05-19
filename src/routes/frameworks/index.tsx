c8516a2206ece7db2040cab80d1270b608ab700c
MGSimard <mgsimard.dev@gmail.com>
2025-05-19 09:11:30 -0400

    Frameworks lingo

--- src/routes/frameworks/index.tsx
+++ src/routes/frameworks/index.tsx
@@ -34,7 +34,7 @@ function PageFrameworks() {
           },
         ]}
         subTitle="Most recommended for both new & seasoned React devs."
-        description="This combination utilizes Vite for its modern build tooling, which employs native ES modules during development for a highly efficient workflow and optimized bundling for production. When paired with TanStack Router's type-safe routing capabilities, it provides a contemporary development experience. This Vite and TanStack Router setup can be scaffolded using `create-tsrouter-app`, offering a modern alternative to the older `create-react-app` (CRA) by focusing on an improved developer experience and performant application builds."
+        description="This combination utilizes Vite for its modern build tooling and TanStack Router's type-safe routing capabilities, it provides an unmatched React development experience. This Vite and TanStack Router setup can be scaffolded using `create-tsrouter-app`, offering a modern alternative to the older `create-react-app` (CRA) by focusing on an improved developer experience and performant application builds. Note that any one of these apps can easily be upgraded to a full-stack framework with TanStack Start (even if all you want is server-side rendering which improves SEO, amongst other things)."
         features={[""]}></Section>
 
       <Section
@@ -51,7 +51,7 @@ function PageFrameworks() {
           { label: "Vite", href: "https://vite.dev/guide/" },
         ]}
         subTitle="Full-stack development, no strings attached."
-        description="TanStack Start is a full-stack framework integrating Vite and various TanStack libraries. It provides comprehensive tools for client and server development. True to its 'no strings attached' philosophy, it avoids proprietary platform lock-in features, offering developers greater freedom in choosing their hosting and infrastructure, unlike Next.js and Vercel."
+        description="TanStack Start is a full-stack framework integrating Vite and various TanStack libraries. It provides comprehensive tools for client and server development. While Next.js has a tendency to take fundamental features away from the developer, pretending like their abstractions are always preferable, TanStack Start on the other hand allows you to retain fine control of everything. Two examples are image preloading for CSS backgrounds and local font implementation, which are miserable at best in Next.js compared to the traditional methods. You'll also find that TanStack Start projects integrate seamlessly with all other TanStack libraries."
         features={[""]}></Section>
 
       <Section
@@ -62,7 +62,7 @@ function PageFrameworks() {
         links={[{ label: "Next.js", href: "https://nextjs.org/" }]}
         docsLinks={[{ label: "Next.js", href: "https://nextjs.org/docs" }]}
         subTitle="The most popular full-stack framework."
-        description="Next.js is a React framework for building full-stack web applications, primarily using its App Router paradigm. This supports server-side rendering with Server Components, static site generation, and client-side rendering. It simplifies page creation with a file-system based router. A key feature is Server Actions, which enable server-side code execution directly within React components, streamlining data mutations and API interactions. For data fetching, the App Router allows direct `async/await` in Server Components and utilizes Route Handlers for creating API endpoints, facilitating seamless integration with any data source or library. Its notable ecosystem makes it suitable for a variety of project scales."
+        description="Next.js is a full-stack React framework maintained by Vercel. It supports server-side rendering with Server Components, static site generation, client-side rendering, and advanced features like Incremental Static Regeneration. Next.js simplifies routing structures through a file-system based router. A key feature is Server Actions, which enable server-side code execution directly within React components, streamlining data mutations and API interactions. While there is a common misconception that using Next.js requires vendor lock-in to Vercel hosting, Next.js can in fact be self-hosted or deployed to a variety of platforms, giving developers flexibility in their deployment choices. Vercel as a platform does provide additional QoL features, but those are not inherently tied to Next.js."
         features={[""]}></Section>
 
       <Section
@@ -73,7 +73,7 @@ function PageFrameworks() {
         links={[{ label: "Payload", href: "https://payloadcms.com/" }]}
         docsLinks={[{ label: "Payload", href: "https://payloadcms.com/docs" }]}
         subTitle="Headless CMS & app framework built on Next.js."
-        description="Payload is a flexible headless CMS and application framework designed for developers. It offers a highly customizable admin interface, robust tools for backend services, granular access control for content and operations, and extensible APIs. This allows for deep integration with various front-end frameworks, such as Next.js, to build tailored content-driven applications."
+        description="Payload is a flexible headless CMS and application framework designed for developers. It offers a highly customizable admin interface, robust tools for backend services, granular access control for content and operations, and extensible APIs. This allows for deep integration with various front-end frameworks, such as Next.js, to build tailored content-driven applications. If you're used to platforms like WordPress, you'll find Payload a much more powerful, performant and hands-on alternative."
         features={[""]}></Section>
 
       <Section
@@ -84,7 +84,7 @@ function PageFrameworks() {
         links={[{ label: "Fumadocs", href: "https://fumadocs.dev/" }]}
         docsLinks={[{ label: "Fumadocs", href: "https://fumadocs.dev/docs/" }]}
         subTitle="Next.js-based documentation generator."
-        description="Fumadocs is a documentation generator using MDX, built on Next.js. It is designed to simplify the creation of documentation websites, focusing on readability and navigation, allowing developers to concentrate on content creation."
+        description="Fumadocs is a documentation generator using MDX, built on Next.js. It is designed to simplify the creation of documentation websites, focusing on readability and navigation, allowing developers to concentrate on writing content rather than code."
         features={[""]}></Section>
     </>
   );
