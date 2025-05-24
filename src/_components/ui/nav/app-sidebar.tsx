c4df522a37c4bafe9ac5f2280a2914dca7dbfd7a
MGSimard <mgsimard.dev@gmail.com>
2025-05-24 18:48:25 -0400

    logo sidebar

--- src/_components/ui/nav/app-sidebar.tsx
+++ src/_components/ui/nav/app-sidebar.tsx
@@ -1,12 +1,12 @@
-import { Sidebar, SidebarContent, SidebarHeader, SidebarTrigger, useSidebar } from "@/_components/ui/sidebar";
+import { Sidebar, SidebarContent } from "@/_components/ui/sidebar";
 import { NavMain } from "@/_components/ui/nav/nav-main";
 import { NavLinks } from "@/_components/ui/nav/nav-links";
-import { NavHead } from "./nav-trigger";
+import { NavHead } from "@/_components/ui/nav/nav-trigger";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" {...props}>
-      <NavHead>Logo</NavHead>
+      <NavHead />
       <SidebarContent>
         <NavMain label={NavLinks.navWebdev.label} items={NavLinks.navWebdev.items} />
         <NavMain label={NavLinks.navCreative.label} items={NavLinks.navCreative.items} />
--- src/_components/ui/nav/nav-trigger.tsx
+++ src/_components/ui/nav/nav-trigger.tsx
@@ -1,14 +1,15 @@
 import { SidebarHeader, SidebarTrigger, useSidebar } from "@/_components/ui/sidebar";
+import { Link } from "@tanstack/react-router";
 
-export function NavHead({ children }: { children: React.ReactNode }) {
+export function NavHead() {
   const { state } = useSidebar();
 
   return (
     <SidebarHeader
-      className={`flex gap-2 p-2 ${
-        state === "collapsed" ? "items-start flex-col" : "items-center flex-row justify-between"
-      }`}>
-      {children}
+      className={`flex gap-2 p-2 items-center justify-between ${state === "collapsed" ? "flex-col" : "flex-row px-4"}`}>
+      <Link to="/" className="shrink-0">
+        <img src="/metadata/icon.svg" alt="Logo" className="overflow-hidden h-6 w-6" />
+      </Link>
       <SidebarTrigger className="hover:cursor-pointer shrink-0" />
     </SidebarHeader>
   );
