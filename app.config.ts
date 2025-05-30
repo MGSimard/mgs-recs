7f159215d32e6fe22d5a747aa2ce5b9223c7f72a
MGSimard <mgsimard.dev@gmail.com>
2025-05-30 19:01:21 -0400

    Static prerendering?

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,10 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
+    prerender: {
+      routes: ["/"],
+      crawlLinks: true,
+    },
     preset: "cloudflare-pages",
     unenv: cloudflare,
   },
