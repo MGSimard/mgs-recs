c603ce85e5104fe50c1a63598a823510612a3f00
MGSimard <mgsimard.dev@gmail.com>
2025-07-01 19:01:54 -0400

    Maybe?

--- src/_components/ui/header/Header.tsx
+++ src/_components/ui/header/Header.tsx
@@ -2,7 +2,7 @@ import { PageH1 } from "@/_components/ui/header/PageH1";
 
 export function Header() {
   return (
-    <header className="flex h-16 shrink-0 items-center gap-2 px-8 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="flex h-12 shrink-0 justify-center items-center gap-2 px-8 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
       <PageH1 />
     </header>
   );
